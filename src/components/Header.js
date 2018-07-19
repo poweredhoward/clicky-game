9d020f4c02212b679dbb3c6fd1ff3057c560f2cc
mhoward <matthew.howard96@yahoo.com>
2018-07-19 12:48:11 -0700

    trying to deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,20 @@ import React from "react";
 const Header = props =>{
     console.log(props)
     return(
-        <p>
-            Score: {props.score}    High Score: {props.high_score}
-        </p>
+        <div>
+            <div className="navbar">
+
+            </div>
+            <div className="jumbotron">
+                <h2>
+                    Score: {props.score}    High Score: {props.high_score}
+                </h2>
+
+                <h3>
+                    Don't click the same picture twice!
+                </h3>
+            </div>
+        </div>
     )
 }
 
